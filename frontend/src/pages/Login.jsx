1106706a09e0397a591c1163db6a66eb348273aa
cpapa116 <cjgiampa@buffalo.edu>
2025-02-12 18:12:49 -0500

    login frontend almost complete besides captcha link

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,27 +1,55 @@
-import React from "react";
+import { React, useState, useEffect} from "react";
+import messageGraphic from "../assets/graphics/messageGraphic.png";
 
 const Login = () => {
+    const [captchaToken, setCaptchaToken] = useState("");
+
+    useEffect (() => {
+        const script = document.createElement("script");
+        script.src = "https://www.google.com/recaptcha/api.js";
+        script.async = true;
+        script.defer = true;
+        document.body.appendChild(script);
+    }, [])
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if(!captchaToken) {
+            alert("Please complete the captcha");
+            return;
+        }
+
+        const formData = newFormData(e.target);
+        formData.append("g-recaptcha-response",captchaToken);
+
+        fetch("backend-endpoint", {
+            method: "POST",
+            body: formData,
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log("Success: ", data);
+        })
+        .catch((error) => {
+            console.error("Error: ", data)
+        })
+    }
+
     return (
         <div className="flex w-full h-screen">
-            <div className="w-3/5 bg-gray-100 p-4">
-                something
+            <div className="w-3/6 p-20 flex justify-center items-center">
+                <img src={messageGraphic} alt="Message Graphic" />
             </div>
-            <div className="w-2/5 p-4 flex justify-center items-center">
-                <div className="p-10 rounded-xl shadow-2xl w-80 flex flex-col gap-4" style={{ backgroundColor: "#2A2A2A" }}>
-                    <h1 className="text-xl font-bold text-white">Login</h1>
-                    <input 
-                        type="text" 
-                        placeholder="Username" 
-                        className="border border-gray-600 p-2 rounded w-full text-white placeholder-gray-400"
-                        style={{backgroundColor: "#443F3F"}}
-                    />
-                    <input 
-                        type="password" 
-                        placeholder="Password" 
-                        className="border border-gray-600 p-2 rounded w-full text-white placeholder-gray-400"
-                        style={{backgroundColor: "#443F3F"}}
-                    />
-                    <button className="w-1/2 text-white p-2 rounded w-full" style={{ backgroundColor: "#1Ac472"}}>Login!</button>
+            <div className="w-3/6 p-4 flex justify-center items-center">
+                <div className="p-14 rounded-xl shadow-2xl w-97 flex flex-col gap-4" style={{backgroundColor: "#2A2A2A"}}>
+                    <h1 className="text-6xl font-bold text-white pb-10">Login</h1>
+                    <form action="your-backend-endpoint" method="POST">
+                        <input type="text" placeholder="Username" className="border border-gray-600 p-2 rounded w-full text-white placeholder-gray-400 mb-10" style={{backgroundColor: "#443F3F"}}/>
+                        <input type="password" placeholder="Password" className="border border-gray-600 p-2 rounded w-full text-white placeholder-gray-400 mb-10" style={{backgroundColor: "#443F3F"}}/>
+                        <div class="g-recaptcha" data-sitekey="YOUR_SITE_KEY"></div>
+                    </form>
+                    <button type="submit" className="w-1/2 text-white p-2 rounded w-full" style={{ backgroundColor: "#1Ac472"}}>Login!</button>
                 </div>
             </div>
         </div>
