0efc1b69681e6f8f900bfe875cceb754d9c910b4
cpapa116 <cjgiampa@buffalo.edu>
2025-02-13 17:55:47 -0500

    login uses site key env

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -5,13 +5,13 @@ const Login = () => {
     const [captchaToken, setCaptchaToken] = useState("");
 
     useEffect (() => {
+        //console.log("logging site key: " + process.env.REACT_APP_SITE_KEY);
         const script = document.createElement("script");
         script.src = "https://www.google.com/recaptcha/api.js";
         script.async = true;
         script.defer = true;
         document.body.appendChild(script);
     }, [])
-
     const handleSubmit = (e) => {
         e.preventDefault();
 
@@ -42,14 +42,14 @@ const Login = () => {
                 <img src={messageGraphic} alt="Message Graphic" />
             </div>
             <div className="w-3/6 p-4 flex justify-center items-center">
-                <div className="p-14 rounded-xl shadow-2xl w-97 flex flex-col gap-4" style={{backgroundColor: "#2A2A2A"}}>
+                <div className="p-14 rounded-xl shadow-2xl w-97 flex flex-col gap-4 bg-[#2A2A2A]">
                     <h1 className="text-6xl font-bold text-white pb-10">Login</h1>
                     <form action="your-backend-endpoint" method="POST">
-                        <input type="text" placeholder="Username" className="border border-gray-600 p-2 rounded w-full text-white placeholder-gray-400 mb-10" style={{backgroundColor: "#443F3F"}}/>
-                        <input type="password" placeholder="Password" className="border border-gray-600 p-2 rounded w-full text-white placeholder-gray-400 mb-10" style={{backgroundColor: "#443F3F"}}/>
-                        <div class="g-recaptcha" data-sitekey="YOUR_SITE_KEY"></div>
+                        <input type="text" placeholder="Username" className="border border-gray-600 p-2 rounded w-full text-white placeholder-gray-400 mb-10 bg-[#443F3F]"/>
+                        <input type="password" placeholder="Password" className="border border-gray-600 p-2 rounded w-full text-white placeholder-gray-400 mb-10 bg-[#443F3F]"/>
+                        <div className="g-recaptcha" data-sitekey={process.env.REACT_APP_SITE_KEY}></div>
                     </form>
-                    <button type="submit" className="w-1/2 text-white p-2 rounded w-full" style={{ backgroundColor: "#1Ac472"}}>Login!</button>
+                    <button type="submit" className="w-1/3 text-white p-2 rounded w-full bg-[#1AC472]">Login</button>
                 </div>
             </div>
         </div>
