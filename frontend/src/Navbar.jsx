453e9743b9c490f2b6dea7a4c3760e90b3ec5fcc
Jack C <jkchin2@buffalo.edu>
2025-02-15 23:08:03 -0500

    added register in with CAPTCHA

--- frontend/src/Navbar.jsx
+++ frontend/src/Navbar.jsx
@@ -4,7 +4,6 @@ import { Link, useLocation, useNavigate } from "react-router-dom";
 const Navbar = () => {
     const navigate = useNavigate();
     const location = useLocation();
-    const navigate = useNavigate();
 
     const handleRegisterClick = () => {
         navigate("/register");
@@ -21,7 +20,7 @@ const Navbar = () => {
         <nav className="text-white flex justify-between bg-[#202020] text-3xl shadow-xl">
             <h1 className="py-3 px-5 sm:ml-5 hover:cursor-pointer">AAAA</h1>
             <div className="flex flex-row text-2xl px-5 sm:mr-2">
-                <h1 className="p-3 hover:cursor-pointer" onClick={handleRegisterClick}>Register</h1>
+                <h1 className="p-3 hover:cursor-pointer" onClick={() => handleRegisterClick()}>Register</h1>
                 <h1 className="py-3">|</h1>
                 <h1 className="p-3 hover:cursor-pointer" onClick={() => navigateLogin()}>Login</h1>
             </div>
--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -8,7 +8,7 @@ const Landing = () => {
     };
 
     return (
-        <div className="flex flex-col h-full text-white sm:pl-10 justify-center sm:justify-start">
+        <div className="flex flex-col h-full text-white sm:px-10 justify-center sm:justify-start">
             <div className="justify-center sm:justify-start text-center sm:text-start">
                 <div className="sm:pt-20 ">
                     <h1 className="text-lg sm:text-2xl md:text-3xl lg:text-4xl">Hello! This is a chatroom for people to chat in!</h1>
@@ -18,8 +18,8 @@ const Landing = () => {
                 </div>
                 <div className="pt-8 sm:pt-16">
                     <h1 className="text-2xl sm:text-3xl sm:pb-5">Sign up to start chatting here!</h1>
-                    <button className="font-casual text-xl sm:text-2xl py-4 px-10 sm:py-6 sm:px-16 mt-3 sm:mt-0 w-fit rounded bg-[#1AC472]">
-                        <h1 onClick={handleRegisterClick}>Sign Up!</h1>
+                    <button className="font-casual text-xl sm:text-2xl py-4 px-10 sm:py-6 sm:px-16 mt-3 sm:mt-0 w-fit rounded bg-[#1AC472]" onClick={handleRegisterClick}>
+                        <h1>Sign Up!</h1>
                     </button>
                 </div>
             </div>
