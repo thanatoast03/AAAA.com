458668c63279099400170ca524427a2840308c7d
cpapa116 <cjgiampa@buffalo.edu>
2025-02-27 11:34:36 -0500

    fixed routing for app hopefully

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,11 +24,10 @@ function ProtectedRoute() {
     useEffect(() => {
         const verifyToken = async() => {
             try{
-                const response = await fetch("/verify", {
+                const response = await fetch("/api/verify", {
                     method: "GET",
                     headers: {
-                        "Authorization" : `Bearer ${sessionStorage.getItem("token")}`,
-                        "Content-Type" : "application/json"
+                        "Authorization" : `Bearer ${sessionStorage.getItem("token")}`
                     }
                 });
 
