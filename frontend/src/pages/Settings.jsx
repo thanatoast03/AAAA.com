6af2a424484c9fd9f897be4a8936261360e68333
cpapa116 <cjgiampa@buffalo.edu>
2025-03-01 17:27:40 -0500

    frontend is ready for changing sensitive info

--- frontend/src/pages/Settings.jsx
+++ frontend/src/pages/Settings.jsx
@@ -6,7 +6,11 @@ import DeleteAccountModal from "../modals/DeleteAccountModal";
 const Settings = () => {
     const [deleteOpen,setDeleteOpen] = useState(false);
     const [newUsername,setNewUsername] = useState("");
-    const [changeStatus,setChangeStatus] = useState("");
+    const [newEmail,setNewEmail] = useState("");
+    const [newPassword,setNewPassword] = useState("");
+    const [changeUsernameStatus,setChangeUsernameStatus] = useState("");
+    const [changeEmailStatus, setChangeEmailStatus] = useState("");
+    const [changePasswordStatus, setChangePasswordStatus] = useState("");
 
 
     const openDeleteModal = () => {
@@ -15,6 +19,18 @@ const Settings = () => {
 
     const closeDeleteModal = () => {
         setDeleteOpen(false);
+    }
+
+    const changeSensitiveInfo = () => {
+        if(newUsername !== ""){
+            changeUsername();
+        }
+        if (newPassword !== ""){
+            changePassword();
+        }
+        if (newEmail !== ""){
+            changeEmail();
+        }
     }
 
     const changeUsername = async() => {
@@ -30,15 +46,64 @@ const Settings = () => {
                 }),
             });
             const data = await response.json();
-            setChangeStatus(data.message);
+            setChangeUsernameStatus(data.message);
+            setNewUsername("");
         } catch (error) {
-            setChangeStatus(error.message);
+            setChangeUsernameStatus(error.message);
+        }
+    }
+
+    const changeEmail = async() => {
+        try{
+            const response = await fetch(process.env.REACT_APP_ACCOUNTS_PATH+"/changeEmail", {
+                method: "POST",
+                headers: {
+                    "Content-Type" : "application/json",
+                    "Authorization" : `Bearer ${sessionStorage.getItem("token")}`,
+                },
+                body: JSON.stringify({
+                    "newEmail" : newEmail,
+                }),
+            });
+            const data = await response.json();
+            setChangeEmailStatus(data.message);
+            setNewEmail("");
+        } catch (error) {
+            setChangeEmailStatus(error.message);
+        }
+    }
+
+    const changePassword = async() => {
+        try{
+            const response = await fetch(process.env.REACT_APP_ACCOUNTS_PATH+"/changePassword", {
+                method: "POST",
+                headers: {
+                    "Content-Type" : "application/json",
+                    "Authorization" : `Bearer ${sessionStorage.getItem("token")}`,
+                },
+                body: JSON.stringify({
+                    "newPassword" : newPassword,
+                }),
+            });
+            const data = await response.json();
+            setChangePasswordStatus(data.message);
+            setNewPassword("");
+        } catch (error) {
+            setChangePasswordStatus(error.message);
         }
     }
 
     const handleUserChange = (e) => {
         setNewUsername(e.target.value); //updates newUsername with input
     };
+
+    const handleEmailChange = (e) => {
+        setNewEmail(e.target.value); //updates newEmail with input
+    };
+
+    const handlePasswordChange = (e) => {
+        setNewPassword(e.target.value); //updates newPassword with input
+    }
 
     return (
         <div className="settingsContainer">
@@ -55,16 +120,18 @@ const Settings = () => {
                             <h1>Username:</h1>
                             <input type="text" placeholder="username" onChange={handleUserChange}/>
                         </div>
+                        <p className="text-[#00FF00]">{changeUsernameStatus}</p>
                         <div className="accountOption">
                             <h1>Password:</h1>
                             <input type="text" placeholder="password"/>
                         </div>
+                        <p className="text-[#00FF00]">{changePasswordStatus}</p>
                         <div className="accountOption">
                             <h1>Email:</h1>
                             <input type="text" placeholder="email"/>
                         </div>
-                        <button className="settingsSave" onClick={() => changeUsername()}>Save Changes</button>
-                        <p className="text-[#00FF00]">{changeStatus}</p>
+                        <p className="text-[#00FF00]">{changeEmailStatus}</p>
+                        <button className="settingsSave" onClick={() => changeSensitiveInfo()}>Save Changes</button>
                         <button className="settingsDelete" onClick={() => openDeleteModal()}>DELETE ACCOUNT</button>
                     </div>
                 </div>
