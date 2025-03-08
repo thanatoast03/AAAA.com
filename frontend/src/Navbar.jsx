4268d41ae1ae32b937716789aafbda7f1aac9c31
jgow12 <jagow2@buffalo.edu>
2025-03-07 22:13:56 -0500

    fixing navbar

--- frontend/src/Navbar.jsx
+++ frontend/src/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+
 
 const Navbar = () => {
     const navigate = useNavigate();
@@ -13,38 +14,39 @@ const Navbar = () => {
 
     const navigateLogin = () => {
         navigate("/login");
-    };
+    }
 
     const navigateSettings = () => {
         navigate("/settings");
-    };
+    }
 
     const navigateChatroom = () => {
-        navigate("/chatroom");
-    };
+        navigate("/chatroom")
+    }
 
     const navigateAdmin = () => {
-        navigate("/admin panel");
-    };
+        navigate("/admin panel")
+    }
 
     const navigateLanding = () => {
-        navigate("/");
-    };
+        navigate("/")
+    }
 
     useEffect(() => {
         const fetchUser = async () => {
             try {
+
                 const token = sessionStorage.getItem("token");
                 if (!token) {
                     throw new Error("No token found");
                 }
 
-                const response = await fetch(`${process.env.REACT_APP_FETCH_PATH}/verify/`, {
+                const response = await fetch(process.env.REACT_APP_FETCH_PATH + '/verify/', {
                     method: "GET",
                     headers: {
                         "Content-Type": "application/json",
-                        Authorization: `Bearer ${token}`,
-                    },
+                        Authorization: `Bearer ${token}`
+                    }
                 });
 
                 if (!response.ok) {
@@ -58,73 +60,70 @@ const Navbar = () => {
                 console.error("Error fetching user:", error);
                 setUser(null);
                 setIsAdmin(false);
-                navigate("/login");
             }
         };
 
         fetchUser();
-    }, [navigate]);
-
-    const renderNavItems = () => {
-        if (user) {
-            return (
-                <>
-                    {location.pathname !== "/settings" && (
-                        <h1 className="p-3 hover:cursor-pointer" onClick={navigateSettings}>
-                            Settings
-                        </h1>
-                    )}
-
-                    {isAdmin && location.pathname !== "/admin panel" && (
-                        <h1 className="p-3 hover:cursor-pointer" onClick={navigateAdmin}>
-                            Admin Panel
-                        </h1>
-                    )}
-                    {location.pathname !== "/chatroom" && (
-                        <h1 className="p-3 hover:cursor-pointer" onClick={navigateChatroom}>
-                            Chatroom
-                        </h1>
-                    )}
-                </>
-            );
-        } else {
-            return (
-                <>
-                    {location.pathname === "/" && (
-                        <>
-                            <h1 className="p-3 hover:cursor-pointer" onClick={handleRegisterClick}>
-                                Register
-                            </h1>
-                            <h1 className="py-3">|</h1>
-                            <h1 className="p-3 hover:cursor-pointer" onClick={navigateLogin}>
-                                Login
-                            </h1>
-                        </>
-                    )}
-
-                    {location.pathname === "/register" && (
-                        <h1 className="p-3 hover:cursor-pointer" onClick={navigateLogin}>
-                            Login
-                        </h1>
-                    )}
-
-                    {location.pathname === "/login" && (
-                        <h1 className="p-3 hover:cursor-pointer" onClick={handleRegisterClick}>
-                            Register
-                        </h1>
-                    )}
-                </>
-            );
-        }
-    };
+    }, []);
 
     return (
         <nav className="text-white flex justify-between bg-[#202020] text-3xl shadow-xl">
-            <h1 className="py-3 px-5 sm:ml-5 hover:cursor-pointer" onClick={navigateLanding}>
-                AAAA
-            </h1>
+            <h1 className="py-3 px-5 sm:ml-5 hover:cursor-pointer" onClick={navigateLanding}>AAAA</h1>
             <div className="flex flex-row text-2xl px-5 sm:mr-2">
-                {renderNavItems()}
+                {user ? (
+                    <>
+                        {location.pathname === '/chatroom' && ( 
+                            <>
+                                {isAdmin && (
+                                    <>
+                                        <h1 className="p-3 hover:cursor-pointer" onClick={navigateAdmin}>Admin Panel</h1>
+                                        <h1 className="py-3">|</h1>
+                                    </>
+                                )}
+                                <h1 className="p-3 hover:cursor-pointer" onClick={navigateSettings}>Settings</h1>
+                            </>
+                        )}
+                        {location.pathname === '/settings' && ( 
+                            <>
+                                {isAdmin && (
+                                    <>
+                                        <h1 className="p-3 hover:cursor-pointer" onClick={navigateAdmin}>Admin Panel</h1>
+                                        <h1 className="py-3">|</h1>
+                                    </>
+                                )}
+                                <h1 className="p-3 hover:cursor-pointer" onClick={navigateChatroom}>Chatroom</h1>
+                            </>
+                        )}
+                        {isAdmin && (
+                            <>
+                                {location.pathname === '/admin panel' && (
+                                    <>
+                                        <h1 className="p-3 hover:cursor-pointer" onClick={navigateChatroom}>Chatroom</h1>
+                                        <h1 className="py-3">|</h1>
+                                        <h1 className="p-3 hover:cursor-pointer" onClick={navigateSettings}>Settings</h1>
+                                    </>
+                                )}
+                            </>
+                        )}
+                    </>
+                ) : (
+                    <>  
+                        {location.pathname === '/' && (
+                            <>
+                                <h1 className="p-3 hover:cursor-pointer" onClick={handleRegisterClick}>Register</h1>
+                                <h1 className="py-3">|</h1>
+                                <h1 className="p-3 hover:cursor-pointer" onClick={navigateLogin}>Login</h1>
+                            </>
+                        )}
+                        {location.pathname === '/register' && (
+                            <h1 className="p-3 hover:cursor-pointer" onClick={navigateLogin}>Login</h1>
+                        )}
+                        {location.pathname === '/login' && (
+                            <h1 className="p-3 hover:cursor-pointer" onClick={handleRegisterClick}>Register</h1>
+                        )}
+                        
+                    </>
+                )}
             </div>
         </nav>
     );
